0057cfa32ec0bac9d762aad67e718f67fbbe3422
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-18 21:28:13 -0300

    c0- create folder and file to traning mongodb

---
+++ mongo.js
@@ -0,0 +1,20 @@
+// remmeber i need require mongodb
+
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+// this serve to adding new data and insights that wants!!!
+MongoClient.connect('mongodb://localhost:27017/data-training', (err,db) => {
+  
+const human = db.collection('human')
+ human.insert({
+     name:'Felipe Martins Programer',
+     age:'20',
+     nacionalites:'Brazilian',
+     email:'machadofelipe2016@outlook.com',
+     gender:'Male'
+ } , (err, res) => {
+     console.log(err)
+ })
+
+})
